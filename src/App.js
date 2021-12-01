629aef462979cbb52a23cc9d4da5292adc2f76e7
Marius <marius@example.com>
2021-12-01 10:05:16 +0200

    proptypes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className='App'>
       <h2>App</h2>
-      <CounterProp name='London' initVal='3' />
+      <CounterProp
+        name='London'
+        initVal={3}
+        specialVal={'special'}
+        selectVal='orange'
+        favNumbers={[7, 12, 75]}
+        arrayOfNumbersOrStrings={[7, 12, 75, '1']}
+      />
     </div>
   );
 }
--- src/components/CounterProp/CounterProp.js
+++ src/components/CounterProp/CounterProp.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
+
 function CounterProp(props) {
   const [localCounterVal, setLocalCounterVal] = useState(props.initVal);
 
@@ -6,8 +8,27 @@ function CounterProp(props) {
     <div>
       <h2>Counter name: {props.name}</h2>
       <h3>Counter initial value: {localCounterVal + 5}</h3>
+      <h2>{props.specialVal}</h2>
+      <h2 style={{ color: props.selectVal }}>{props.selectVal}</h2>
+      <h4>
+        {props.favNumbers.map((n) => (
+          <span key={n}>{n + 2} -- </span>
+        ))}
+      </h4>
     </div>
   );
 }
+
+// check props
+CounterProp.propTypes = {
+  name: PropTypes.string.isRequired,
+  initVal: PropTypes.number.isRequired,
+  specialVal: PropTypes.node.isRequired,
+  selectVal: PropTypes.oneOf(['red', 'green', 'blue', 'orange']),
+  favNumbers: PropTypes.arrayOf(PropTypes.number),
+  arrayOfNumbersOrStrings: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+  ),
+};
 
 export default CounterProp;
